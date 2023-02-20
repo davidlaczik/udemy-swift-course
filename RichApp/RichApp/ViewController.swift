8a8cfadefdbb72cf4be2f5414a5f25746262e1da
Laczik Dávid <laczik.david@outlook.com>
2023-02-20 03:19:20 +0100

    Add - I Am Rich app

---
+++ RichApp/RichApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RichApp
+//
+//  Created by Dávid Laczik on 2023. 02. 20..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
